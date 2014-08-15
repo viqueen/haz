46423a3aac7b72eb00c4809267e3fb7587fe84b2
Hasnae Rehioui <hasnae.rehioui@gmail.com>
2014-08-15 16:09:46 +0200

    #6 yet another test case

--- data/expr/src/test/java/com/haz/data/expr/ArithmeticExprEvaluatorTestCase.java
+++ data/expr/src/test/java/com/haz/data/expr/ArithmeticExprEvaluatorTestCase.java
@@ -15,21 +15,27 @@ import org.junit.Test;
  */
 public class ArithmeticExprEvaluatorTestCase {
 
-  private static final ArithmeticExprEvaluator evaluator = new ArithmeticExprEvaluator();
-  private static final String                  EXPR_ONE  = "3 * 2 + 4 * 2";
-  private static final String                  EXPR_TWO  = "3 * (4 + 1)";
+  private static final ArithmeticExprEvaluator evaluator  = new ArithmeticExprEvaluator();
+  private static final String                  EXPR_ONE   = "3 * 2 + 4 * 2";
+  private static final String                  EXPR_TWO   = "3 * (4 + 1)";
+  private static final String                  EXPR_THREE = "(2 + 2) * 4";
 
   @Test
   public void testSumExpression() {
     Optional<Double> resultOne = evaluator.eval(EXPR_ONE);
     assertTrue(resultOne.isPresent());
-    assertTrue(String.format("expected 21 but was %s", resultOne.get()), resultOne
-        .get().equals(14.0));
+    assertTrue(String.format("expected 14 but was %s", resultOne.get()),
+        resultOne.get().equals(14.0));
 
     Optional<Double> resultTwo = evaluator.eval(EXPR_TWO);
     assertTrue(resultTwo.isPresent());
-    assertTrue(String.format("expected 21 but was %s", resultTwo.get()), resultTwo
-        .get().equals(15.0));
+    assertTrue(String.format("expected 15 but was %s", resultTwo.get()),
+        resultTwo.get().equals(15.0));
+
+    Optional<Double> resultThree = evaluator.eval(EXPR_THREE);
+    assertTrue(resultThree.isPresent());
+    assertTrue(String.format("expected 16 but was %s", resultThree.get()),
+        resultThree.get().equals(16.0));
 
   }
 }
