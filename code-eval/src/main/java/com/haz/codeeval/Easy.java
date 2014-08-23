78c75693ecd2b16b90023bc822170d598f1d38d8
Hasnae Rehioui <hasnae.rehioui@gmail.com>
2014-08-23 23:12:15 +0200

    #7 basic stuff

--- code-eval/src/main/java/com/haz/codeeval/Easy.java
+++ code-eval/src/main/java/com/haz/codeeval/Easy.java
@@ -13,19 +13,29 @@ public class Easy {
     boolean[] isNotPrime = new boolean[pLimit];
     isNotPrime[0] = true;
     int sum = 0;
-    int count = 0;
     for (int value = 2; value <= pLimit; value++) {
       if (!isNotPrime[value - 1]) {
         sum += value;
-        count++;
         for (int mult = 2; mult * value <= pLimit; mult++) {
           isNotPrime[(mult * value) - 1] = true;
         }
       }
     }
-    System.err.println(count);
     return sum;  
   }
   
+  public static int sumOfDigits(String pStr) {
+	  int sum = 0;
+    for (char c : pStr.toCharArray()) {
+	    sum += (c - '0');
+	  }
+    return sum;
+  }
   
+  public static int multiplesOfaNumber(int pX, int pN) {
+    int result = 0;
+    int multiplier = 1;    
+    while ((result = pN * multiplier++) < pX);
+    return result;
+  }
 }
--- code-eval/src/test/java/com/haz/codeeval/EasyTestCase.java
+++ code-eval/src/test/java/com/haz/codeeval/EasyTestCase.java
@@ -17,4 +17,16 @@ public class EasyTestCase {
     assertEquals(17, Easy.primeSum(10));
     assertEquals(3682913, Easy.primeSum(7925));     
   }
+  
+  @Test
+  public void testSumOfDigits() {
+    assertEquals(5, Easy.sumOfDigits("23"));
+    assertEquals(19, Easy.sumOfDigits("496"));    
+  }
+  
+  @Test
+  public void testMultiplesOfaNumber() {
+    assertEquals(16, Easy.multiplesOfaNumber(13, 8));
+    assertEquals(32, Easy.multiplesOfaNumber(17, 16));    
+  }
 }
