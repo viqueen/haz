942b630d2480f8b4ac717302837fade5cfde1265
Hasnae Rehioui <hasnae.rehioui@gmail.com>
2014-08-24 21:28:27 +0200

    #7 some more random stuff

--- code-eval/src/main/java/com/haz/codeeval/Easy.java
+++ code-eval/src/main/java/com/haz/codeeval/Easy.java
@@ -3,6 +3,10 @@
  */
 package com.haz.codeeval;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author hasnaer
  *
@@ -21,21 +25,65 @@ public class Easy {
         }
       }
     }
-    return sum;  
-  }
-  
-  public static int sumOfDigits(String pStr) {
-	  int sum = 0;
-    for (char c : pStr.toCharArray()) {
-	    sum += (c - '0');
-	  }
     return sum;
   }
-  
+
+  public static int sumOfDigits(String pStr) {
+    int sum = 0;
+    for (char c : pStr.toCharArray()) {
+      sum += (c - '0');
+    }
+    return sum;
+  }
+
   public static int multiplesOfaNumber(int pX, int pN) {
     int result = 0;
-    int multiplier = 1;    
-    while ((result = pN * multiplier++) < pX);
+    int multiplier = 1;
+    while ((result = pN * multiplier++) < pX)
+      ;
     return result;
+  }
+
+  public static String uniqueElements(String pStr) {
+    if (pStr != null && !pStr.isEmpty()) {
+      StringBuilder builder = new StringBuilder();
+      String[] tokens = pStr.split(",");
+      String previous = "";
+      for (String token : tokens) {
+        if (!token.equals(previous)) {
+          builder.append(token).append(",");
+          previous = token;
+        }
+      }
+      return builder.substring(0, builder.length() - 1);
+    }
+    return "";
+  }
+  
+  public static String simpleSorting(String pStr) {
+    StringBuilder builder = new StringBuilder();
+    String[] tokens = pStr.split(" ");
+    List<SimpleSortingData> data = new ArrayList<>();
+    for (String token : tokens) {
+      data.add(new SimpleSortingData(token));
+    }
+    Collections.sort(data);
+    for (SimpleSortingData d : data) {
+      builder.append(d.string).append(" ");
+    }
+    return builder.toString().trim();
+  }
+  
+  private static class SimpleSortingData implements Comparable<SimpleSortingData> {
+    String string;
+    Double value;    
+    public SimpleSortingData(String pStr) {
+      string = pStr;
+      value = Double.parseDouble(pStr);
+    }
+    @Override
+    public int compareTo(SimpleSortingData pOther) {
+      return value.compareTo(pOther.value);
+    }
   }
 }
--- code-eval/src/test/java/com/haz/codeeval/EasyTestCase.java
+++ code-eval/src/test/java/com/haz/codeeval/EasyTestCase.java
@@ -13,20 +13,36 @@ import static org.junit.Assert.*;
 public class EasyTestCase {
 
   @Test
-  public void testPrimeSum () {
+  public void testPrimeSum() {
     assertEquals(17, Easy.primeSum(10));
-    assertEquals(3682913, Easy.primeSum(7925));     
+    assertEquals(3682913, Easy.primeSum(7925));
   }
-  
+
   @Test
   public void testSumOfDigits() {
     assertEquals(5, Easy.sumOfDigits("23"));
-    assertEquals(19, Easy.sumOfDigits("496"));    
+    assertEquals(19, Easy.sumOfDigits("496"));
   }
-  
+
   @Test
   public void testMultiplesOfaNumber() {
     assertEquals(16, Easy.multiplesOfaNumber(13, 8));
-    assertEquals(32, Easy.multiplesOfaNumber(17, 16));    
+    assertEquals(32, Easy.multiplesOfaNumber(17, 16));
+  }
+
+  @Test
+  public void testUniqueElements() {
+    assertEquals("1,2,3,4", Easy.uniqueElements("1,1,2,3,4,4,4,4"));
+    assertEquals("1", Easy.uniqueElements("1,1,1,1,1"));
+    assertEquals("", Easy.uniqueElements(""));
+  }
+
+  @Test
+  public void testSimpleSorting() {
+    assertEquals("-38.797 7.581 14.354 70.920 90.374 99.323",
+        Easy.simpleSorting("70.920 -38.797 14.354 99.323 90.374 7.581"));
+    assertEquals("-55.552 -37.507 -3.263 27.999 40.079 65.213",
+        Easy.simpleSorting("-55.552 -37.507 -3.263 27.999 40.079 65.213"));
+
   }
 }
